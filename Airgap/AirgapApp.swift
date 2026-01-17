065e7c1aa27b7b894cb7975679611898b9d96ea8
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-01-17 14:48:21 +0000

    Initial Commit

---
+++ Airgap/AirgapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirgapApp.swift
+//  Airgap
+//
+//  Created by Evan Plant on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AirgapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
